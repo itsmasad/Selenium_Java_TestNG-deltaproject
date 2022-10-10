c8b9b038f2f1df89520bb47b5efda9e119fd621f
itsmasad <Muhammadasadofficial@gmail.com>
2022-10-10 05:49:09 +0500

    Some changes

--- src/main/java/eurosoft/com/App.java
+++ src/main/java/eurosoft/com/App.java
@@ -1,6 +1,7 @@
 package eurosoft.com;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -14,7 +15,29 @@ public final class App {
         WebDriverManager.chromedriver().setup();
  
          WebDriver driver = new ChromeDriver();
-         driver.get("https://www.youtube.com");
+
+
+
+
+          //Maximize The Window
+        driver.manage().window().maximize();
+
+        // Navigating to Url
+        driver.get("https://cabtreasurecloud5.com/Delta_Demo/Authentication/Index");
+
+        // Login 
+        driver.findElement(By.xpath("//*[@id='UserName']")).sendKeys("khawar@eurosofttech.co.uk");
+        driver.findElement(By.xpath("//*[@id='Password']")).sendKeys("CD5S1wSKASV#Umc");
+        driver.findElement(By.xpath("//*[@id='btnLogin']")).click();
+
+        System.out.println(driver.getTitle());
+
+
+
+
+        
+
+        // driver.quit();
  
      }   
 
